3fd4fe038711fe48401c41c383e5c58a087a8bc0
elleryq <elleryq@gmail.com>
2012-10-05 18:04:18 +0800

    Use scraperwiki.com as source.

--- haodoo.py
+++ haodoo.py
@@ -40,94 +40,43 @@ class HaodooStore(StorePlugin):
         
     def search(self, query, max_results=10, timeout=60):
         print( "search!")
-        # if json is None:
-        #     return None
-        key = 'AIzaSyBRSleYvSyG-Bxo9_6-4TJalmkw77jMzmo'
-        cx = '015871176235127732549:or0582yikva'
         q = query.decode('utf-8')
-        
-        # default is unicode string, so we encode as utf-8 after string is formated.
-        url = "https://www.googleapis.com/customsearch/v1?" + urlencode( {
-            "key": key,
-            "cx": cx,
-            "q": q
-            } )
+
+        url = "https://views.scraperwiki.com/run/haodooscraperview/?" + urlencode(
+                {
+                    "q": q
+                } )
+        #url = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?" + urlencode(
+        #        {
+        #            "format": "jsondict",
+        #            "name": "haodooscraper",
+        #            "query": "select * from `bookpages` where title like '%{0}%' or volumes like '%{0}%' limit 10".format( q ),
+        #        } )
         print( url )
 
         br = browser()
-
         with closing(br.open(url, timeout=timeout)) as f:
             json_doc = f.read()
-            results = json.loads( json_doc )
-            for data in results['items']:
-                s = SearchResult()
-                s.title = data["title"].strip()
-                s.detail_item = data["link"].strip()
-                s.price = '$0.00'
-                s.drm = SearchResult.DRM_UNLOCKED
+            if len(json_doc)>0:
+                result = json.loads( json_doc )
+                for book in result:
+                    if not book.has_key('volumes') or not book['volumes']:
+                        print( "No volumes, skip." )
+                        continue
+                    volumes = json.loads( book['volumes'] )
+                    print( type( volumes ) )
+                    for volume in volumes:
+                        s = SearchResult()
+                        s.title = volume['title']
+                        s.detail_item = book['url']
+                        s.price = '$0.00'
+                        s.drm = SearchResult.DRM_UNLOCKED
 
-                yield s
-
-    def get_details(self, search_result, timeout):
-        print("get_details")
-        try:
-            print( search_result.detail_item )
-        except:
-            pass
-
-        url = url_slash_cleaner( search_result.detail_item )
-        br = browser()
-        with closing(br.open(url, timeout=timeout*2)) as nf:
-            content = nf.read()
-            doc = html.fromstring(content)
-
-            for save_item in doc.xpath('//input[contains(@type, "button")]'):
-                onclick = save_item.get('onclick')
-                if "DownloadEpub" in onclick:
-                    self.__handle( search_result, onclick, "epub" )
-                elif "DownloadUpdb" in onclick:
-                    self.__handle( search_result, onclick, "updb" )
-                elif "DownloadPdb" in onclick:
-                    self.__handle( search_result, onclick, "pdb" )
-
-        return True
-
-    def __find_book_id( self, onclick ):
-        # find which kind of quote, ' or "
-        quote = "'"
-        try:
-            start = onclick.index( quote )
-        except ValueError: 
-            quote = '"'
-
-        book_id = ''
-        try:
-            start = onclick.index( quote )
-            end = onclick.rindex( quote )
-            book_id = onclick[ start+1:end ]
-        except:
-            pass
-
-        return book_id
-
-    def __convert_to_dl_url( self, book_id, ext ):
-        result = "http://www.haodoo.net/?" + urlencode( {
-            "M": "d",
-            "P": book_id+"." + ext } )
-        #print( "__convert_to_dl_url()=%s" % result )
-        return result
-
-    def __handle( self, search_result, onclick, book_type ):
-        # retrieve only the parameter in javascript.
-        book_id = self.__find_book_id( onclick )
-        if not book_id:
-            print( "Cannot extract book_id" )
-            return
-
-        # convert to download link
-        dl_link = self.__convert_to_dl_url( book_id, book_type )
-
-        # put in search result.
-        search_result.downloads[book_type] = dl_link
-        search_result.formats = ', '.join(search_result.downloads.keys())
+                        if volume.has_key('type') and len(volume["type"]):
+                            exts = json.loads( volume["type"] )
+                            for t in exts:
+                                ext, dl_link = t
+                                s.downloads[ext] = dl_link
+                            s.formats = ', '.join(s.downloads.keys())
+                        yield s
 
